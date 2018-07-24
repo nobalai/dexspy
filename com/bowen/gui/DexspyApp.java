d5291c3b0ef8b9056980f4d615aca6b1ffeca02d
nobalai <nobal.lai@gmail.com>
2018-07-25 00:55:58 +0800

    Add Menu

--- com/bowen/gui/DexspyApp.java
+++ com/bowen/gui/DexspyApp.java
@@ -1,24 +1,22 @@
 package com.bowen.gui;
 
+import com.sun.istack.internal.NotNull;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DexspyApp extends Application {
 
     private static final String TITLE = "Dex file inspector";
 
-    private void initializeMenu(StackPane root) {
-        MenuBar menuBar = new MenuBar();
-        Menu fileMenu = new Menu("File");
-        Menu helpMenu = new Menu("Help");
-        menuBar.getMenus().addAll(fileMenu, helpMenu);
-
-        root.getChildren().addAll(menuBar);
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
@@ -27,17 +25,30 @@ public class DexspyApp extends Application {
         button.setText("Say hello world");
         button.setOnAction(event -> System.out.println("Hello World!"));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(button);
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(button);
 
-        initializeMenu(root);
+        VBox vBox = new VBox();
+        initializeMenu(vBox);
+        vBox.getChildren().add(stackPane);
 
         primaryStage.setTitle(TITLE);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(vBox, 300, 250));
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        Application.launch(args);
+    private void initializeMenu(@NotNull VBox vBox) {
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        MenuItem exit = new MenuItem("Exit");
+        exit.setOnAction(event -> System.exit(0));
+        fileMenu.getItems().addAll(exit);
+
+        Menu helpMenu = new Menu("Help");
+        MenuItem about = new MenuItem("About");
+        helpMenu.getItems().addAll(about);
+
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+        vBox.getChildren().addAll(menuBar);
     }
 }
