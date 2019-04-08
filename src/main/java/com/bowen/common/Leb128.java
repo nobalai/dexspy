4f0bd84f7b37fa9779f5554752e339f0d7814944
nobal <nobal.lai@gmail.com>
2019-04-08 14:24:49 +0800

    Implement Leb128 read and write

--- src/main/java/com/bowen/common/Leb128.java
+++ src/main/java/com/bowen/common/Leb128.java
@@ -1,5 +1,7 @@
 package com.bowen.common;
 
+import java.nio.ByteBuffer;
+
 public final class Leb128 {
 
   private Leb128() {}
@@ -45,4 +47,83 @@ public final class Leb128 {
     return count;
   }
 
+  /* Reads an unsigned integer from {@code buf}. */
+  public static int readUnsignedLeb128(ByteBuffer buf) {
+    int result = 0;
+    int cur;
+    int count = 0;
+
+    do {
+      cur = buf.get() & 0xff;
+      result |= (cur & 0x7f) << (count * 7);
+      count++;
+    } while (((cur & 0x80) == 0x80) && count < 5);
+
+    if ((cur & 0x80) == 0x80) {
+      throw new RuntimeException("invalid LEB128 sequence.");
+    }
+
+    return result;
+  }
+
+  /* Reads an signed integer from {@code buf}. */
+  public static int readSignedLeb128(ByteBuffer buf) {
+    int result = 0;
+    int cur;
+    int count = 0;
+    int signBits = -1;
+
+    do {
+      cur = buf.get() & 0xff;
+      result |= (cur & 0x7f) << (count * 7);
+      signBits <<= 7;
+      count++;
+    } while ((cur & 0x80) == 0x80 && count < 5);
+
+    if ((cur & 0x80) == 0x80) {
+      throw new RuntimeException("invalid LEB128 sequence.");
+    }
+
+    // Sign extend if appropriate
+    if (((signBits >> 1) & result) != 0) {
+      result |= signBits;
+    }
+  
+    return result;
+  }
+
+  /**
+   * Writes {@code value} as an unsigned integer to {@code buf}, starting at
+   * {@code position}. Returns the number of bytes written.
+   */
+  public static void writeUnsignedLeb128(ByteBuffer buf, int value) {
+    int remaining = value >>> 7;
+  
+    while (remaining != 0) {
+        buf.put((byte) ((value & 0x7f) | 0x80));
+        value = remaining;
+        remaining >>>= 7;
+    }
+  
+    buf.put((byte) (value & 0x7f));
+  }
+
+  /**
+   * Writes {@code value} as a signed integer to {@code buf}, starting at
+   * {@code position}. Returns the number of bytes written.
+   */
+  public static void writeSignedLeb128(ByteBuffer buf, int value) {
+    int remaining = value >> 7;
+    boolean hasMore = true;
+    int end = ((value & Integer.MIN_VALUE) == 0) ? 0 : -1;
+
+    while (hasMore) {
+        hasMore = (remaining != end)
+                || ((remaining & 1) != ((value >> 6) & 1));
+
+        buf.put((byte) ((value & 0x7f) | (hasMore ? 0x80 : 0)));
+        value = remaining;
+        remaining >>= 7;
+    }
+  }
 }
--- src/test/java/com/bowen/common/Leb128Test.java
+++ src/test/java/com/bowen/common/Leb128Test.java
@@ -3,10 +3,17 @@ package com.bowen.common;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-import org.junit.Test;
+import java.nio.BufferOverflowException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class Leb128Test {
-  
+
   @Test
   public void unsignedSize() {
     int size1 = 0b1000000;
@@ -29,5 +36,57 @@ public class Leb128Test {
 
     int negativeSize2 = -size2;
     assertThat(Leb128.signedLeb128Size(negativeSize2), is(1));
+  }
+
+  @Test
+  public void readUnsignedValue() {
+    ByteBuffer oneByteBuf = ByteBuffer.wrap(new byte[] {0b01000000});
+    ByteBuffer twoBytesBuf = ByteBuffer.wrap(new byte[] {(byte) 0b11000000, 0b01000000}); // LSB
+
+    assertThat(Leb128.readUnsignedLeb128(oneByteBuf), is(0b01000000));
+    assertThat(Leb128.readUnsignedLeb128(twoBytesBuf), is(0b010000001000000));
+  }
+
+  @Test
+  public void readSignedValue() {
+    ByteBuffer oneByteBuf = ByteBuffer.wrap(new byte[] {0b01000000});
+    ByteBuffer twoBytesBuf = ByteBuffer.wrap(new byte[] {(byte) 0b11000000, 0b01000000});
+
+    assertThat(Leb128.readSignedLeb128(oneByteBuf), is(0b11000000 | 0xFFFFFF00));
+    assertThat(Leb128.readSignedLeb128(twoBytesBuf), is(0b1110000001000000 | 0xFFFF0000));
+  }
+
+  @Test
+  public void writeUnsignedValue() throws BufferOverflowException {
+    ByteBuffer oneByteBuf = ByteBuffer.allocate(1);
+
+    Leb128.writeUnsignedLeb128(oneByteBuf, 0b01000000);
+    oneByteBuf.rewind();
+
+    assertThat(oneByteBuf.get(), is((byte) 0b01000000));
+
+    ByteBuffer twoBytesBuf = ByteBuffer.allocate(2);
+
+    Leb128.writeUnsignedLeb128(twoBytesBuf, 0b010000001000000);
+    twoBytesBuf.rewind();
+
+    assertThat(twoBytesBuf.order(ByteOrder.LITTLE_ENDIAN).getShort(), is((short) 0b0100000011000000));
+  }
+
+  @Test
+  public void writeSignedValue() {
+    ByteBuffer twoBytesBuf = ByteBuffer.allocate(2);
+
+    Leb128.writeSignedLeb128(twoBytesBuf, 0b01000000);
+    twoBytesBuf.rewind();
+
+    assertThat(twoBytesBuf.order(ByteOrder.LITTLE_ENDIAN).getShort(), is((short) 0b011000000));
+
+    ByteBuffer fourBytesBuf = ByteBuffer.allocate(4);
+
+    Leb128.writeSignedLeb128(fourBytesBuf, 0b10000001000000);
+    fourBytesBuf.rewind();
+
+    assertThat(fourBytesBuf.order(ByteOrder.LITTLE_ENDIAN).getInt(), is(0b01100000011000000));
   }
 }
